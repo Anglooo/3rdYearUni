06607311d07a3ea99b44c0155d2c2e6a7a29f04b
anglooo <tommy.frow@gmail.com>
2013-10-30 10:42:36 +0000

    added compile command

---
+++ CG_CW_Letters/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+echo "compile time"
+
+gcc ./drawD.cpp ./drawT.cpp ./drawM.cpp ./drawMM.cpp ./drawF.cpp ./drawC.cpp ./cube.cpp ./driverFish.cpp  -o Fish -lGLU -lglut -lGL -lm^C
