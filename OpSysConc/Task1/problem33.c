aeb342b7d99c934d25d346dad8dee511462ea6ef
Thomas Frow <tommy.frow@gmail.com>
2014-02-05 12:12:54 +0000

    added task2

--- OpSysConc/Task1/problem33.c
+++ OpSysConc/Task1/problem33.c
@@ -5,27 +5,43 @@
 
 int main(int argc, char *argv[])
 {
-
-	pid_t p = fork();
-
-	if(p == -1){
+	
+	
+	int pid;	
+	pid = fork();
+	char test  = argv[0];
+	
+	if(pid == -1){
 		exit(EXIT_FAILURE);
 	}
 
-	if(p == 0){
-		execve("/bin/ls", argv, NULL);
-		return(1);
-	}
-	int result;
-
-	waitpid(p,&result,0);
-
-	if(result == 0)
+	if(pid == 0)
 	{
-		printf("Success\n");
+		int pipefd[2];
+		if (pipe(pipefd)) {
+			perror("pipe");
+			exit(127);
+		}
+		 
+		 switch (fork()) {
+    case -1:
+        perror("fork");
+    case 0:
+          
+        close(pipefd[0]); 
+        dup2(pipefd[1], 1);  
+        close(pipefd[1]); 
+        execvp("ls", argv);
+        exit(126);
+    default:
+     
+        close(pipefd[1]);  
+        dup2(pipefd[0], 0);  
+        close(pipefd[0]);  
+        execvp(test, argv);
+        exit(125);
+    }
 	}
-	else{
-		printf("Error\n");
-	}
-
+	
+return(0);	 
 }
